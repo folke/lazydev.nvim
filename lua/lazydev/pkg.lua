95aabb27a0a8fec9826c6ca45ff8ba3d886a8888
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-01 22:58:34 +0200

    perf(pkg): cache unloaded packs for packpath impl

--- lua/lazydev/pkg.lua
+++ lua/lazydev/pkg.lua
@@ -10,19 +10,25 @@ function M.lazy_unloaded(modname)
   return Util.get_unloaded_rtp(modname)
 end
 
+---@type string[]
+local packs = nil
+
 ---@param modname string
 function M.pack_unloaded(modname)
+  if packs then
+    return packs
+  end
+
   local sites = vim.opt.packpath:get() ---@type string[]
   local default_site = vim.fn.stdpath("data") .. "/site"
   if not vim.tbl_contains(sites, default_site) then
     sites[#sites + 1] = default_site
   end
 
-  local packs = {} ---@type string[]
+  packs = {} ---@type string[]
   for _, site in pairs(sites) do
     for _, pack in ipairs(vim.fn.expand(site .. "/pack/*/opt/*/lua", false, true)) do
       if not pack:find("*", 1, true) then
-        packs[#packs + 1] = pack:sub(1, -5)
         packs[#packs + 1] = vim.fs.normalize(pack:sub(1, -5))
       end
     end
