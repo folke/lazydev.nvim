7008928168c63bc3ddc1e1aa2ec27b3591670ae8
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-11-01 16:48:20 +0100

    fix(completion): show top mods when no module typed yet

--- lua/lazydev/pkg.lua
+++ lua/lazydev/pkg.lua
@@ -1,8 +1,8 @@
 ---@class lazydev.Pkg
 local M = {}
 
-M.PAT_MODULE_BASE = "%-%-%-%s*@module%s*[\"']([%w%.%-_/]+)"
-M.PAT_REQUIRE_BASE = "require%s*,?%s*%(?%s*['\"]([%w%.%-_/]+)"
+M.PAT_MODULE_BASE = "%-%-%-%s*@module%s*[\"']([%w%.%-_/]*)"
+M.PAT_REQUIRE_BASE = "require%s*,?%s*%(?%s*['\"]([%w%.%-_/]*)"
 M.PAT_MODULE_BEFORE = M.PAT_MODULE_BASE .. "$"
 M.PAT_REQUIRE_BEFORE = M.PAT_REQUIRE_BASE .. "$"
 M.PAT_MODULE = M.PAT_MODULE_BASE .. "[\"']"
